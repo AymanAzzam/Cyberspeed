ecd655ef9cc572efa3f7ce6c029d64dbb2c137c6
Ayman Azzam <aymanazzam63@gmail.com>
2024-03-18 23:17:22 +0400

    Add the application code

---
+++ App/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+from flask.logging import create_logger
+import logging
+
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+
+@app.route('/')
+def welcome():
+    return render_template('form.html')
+
+
+@app.route('/', methods=['POST'])
+def result():
+    x = request.form.get("x", type=int, default=0)
+    y = request.form.get("y", type=int, default=0)
+    operation = request.form.get("operation")
+    LOG.info("x = %s, y = %s, operation = %s\n", x, y, operation)
+    if(operation == 'Addition'):
+        result = x + y
+    elif(operation == 'Subtraction'):
+        result = x - y
+    elif(operation == 'Multiplication'):
+        result = x * y
+    elif(operation == 'Division'):
+    	if(x==0 and y==0):
+    		result = 0
+    	else:
+        	result = x / y
+    else:
+        result = 0
+    result
+    return render_template('form.html', entry=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
